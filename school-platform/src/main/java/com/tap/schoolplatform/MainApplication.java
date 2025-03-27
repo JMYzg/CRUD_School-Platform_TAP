56fe79e56607d6b3850fb9f69941b843de644c10
Brisa <brisabcp@gmail.com>
2025-03-27 15:33:52 -0700

    Add new degree and group views

--- school-platform/src/main/java/com/tap/schoolplatform/MainApplication.java
+++ school-platform/src/main/java/com/tap/schoolplatform/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication extends Application {
     @Override
 
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/admin-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/LoginFT-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 879, 656);
         stage.setScene(scene);
         stage.show();
