477afa383faa597f2fda0fb3d26e000a5a4ffb0d
GaryJP <juarezpalmagary@gmail.com>
2025-03-27 16:39:58 -0700

    add a test function for login

--- school-platform/src/main/java/com/tap/schoolplatform/MainApplication.java
+++ school-platform/src/main/java/com/tap/schoolplatform/MainApplication.java
@@ -11,7 +11,7 @@ public class MainApplication extends Application {
     @Override
 
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/LoginFT-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/views/Login-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 879, 656);
         stage.setScene(scene);
         stage.show();
