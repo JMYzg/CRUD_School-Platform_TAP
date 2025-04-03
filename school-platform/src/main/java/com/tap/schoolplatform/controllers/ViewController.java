ab0700bc002fd8c1d0c3216dcb324c88f9915bd2
JMYzg <150627399+JMYzg@users.noreply.github.com>
2025-04-03 00:30:01 -0700

    Attempt merge

--- school-platform/src/main/java/com/tap/schoolplatform/controllers/ViewController.java
+++ school-platform/src/main/java/com/tap/schoolplatform/controllers/ViewController.java
@@ -2,13 +2,21 @@ package com.tap.schoolplatform.controllers;
 
 import com.tap.schoolplatform.models.shared.Address;
 import com.tap.schoolplatform.models.shared.BirthDate;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.time.LocalDate;
 import java.util.Objects;
 
 public abstract class ViewController {
@@ -20,12 +28,12 @@ public abstract class ViewController {
         alertWindow.showAndWait();
     }
 
-    public BirthDate createBrithDate (TextField dayTF, TextField monthTF, TextField yearTF) {
-        int d = Integer.parseInt(dayTF.getText());
-        int m = Integer.parseInt(monthTF.getText());
-        int y = Integer.parseInt(yearTF.getText());
-
-        return new BirthDate(d, m, y);
+    public BirthDate createBrithDate (DatePicker datePicker) {
+        LocalDate date = datePicker.getValue();
+        System.out.println(date.getDayOfMonth());
+        System.out.println(date.getMonth().getValue());
+        System.out.println(date.getYear());
+        return new BirthDate(date.getDayOfMonth(), date.getMonthValue(), date.getYear());
     }
 
     public Address createAddress(TextField street, TextField PC, TextField colony, TextField city, TextField state, TextField country){
@@ -36,11 +44,34 @@ public abstract class ViewController {
     public void loadPageView(String pageName, BorderPane borderPane) {
         Parent root;
         try {
-            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource(pageName)));
+            root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/" + pageName + ".fxml")));
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
         borderPane.setCenter(root);
+    }
+
+    public void loadNewPageView (ActionEvent event, String viewName, String viewTitle) throws IOException {
+        Stage ownerStage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        Stage primaryStage = new Stage();
+        primaryStage.initModality(Modality.WINDOW_MODAL);
+        primaryStage.initOwner(ownerStage);
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/" + viewName + ".fxml")));
+        primaryStage.setTitle(viewTitle);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
+        primaryStage.setResizable(false);
+    }
+
+    public void logOutFunction (Button logOutButton) throws IOException {
+        Stage stage = (Stage) logOutButton.getScene().getWindow();
+        stage.close();
+        Stage primaryStage = new Stage();
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/views/login-view.fxml")));
+        primaryStage.setTitle("Log in");
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.show();
     }
 
 }
