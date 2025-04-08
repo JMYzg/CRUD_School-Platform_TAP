d06799248d7775256e33049a6aa081baf23b83ba
Brisa <brisabcp@gmail.com>
2025-04-07 20:20:36 -0700

    Add new stages

--- school-platform/src/main/java/com/tap/schoolplatform/controllers/teacherControllers/examTeacherViewControllers/ExamOptionController.java
+++ school-platform/src/main/java/com/tap/schoolplatform/controllers/teacherControllers/examTeacherViewControllers/ExamOptionController.java
@@ -3,14 +3,31 @@ package com.tap.schoolplatform.controllers.teacherControllers.examTeacherViewCon
 import com.tap.schoolplatform.controllers.ViewController;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 
 import java.io.IOException;
 
 public class ExamOptionController extends ViewController {
 
     public Button createNewExamButton;
+    private RadioButton radioButton1;
+    private RadioButton radioButton2;
+    private RadioButton radioButton3;
+    private RadioButton radioButton4;
+
+    private TextField option1;
+    private TextField option2;
+    private TextField option3;
+    private TextField option4;
+
+    private ToggleGroup radioGroup;
 
     public void createNewExamn(ActionEvent event) throws IOException {
         loadNewPageView(event, "/views/teacher-views/teacher-option-exam-new-view.fxml", "Create new exam");
+       // radioButtonSelected();
     }
+
+    private void radioButtonSelected() {}
 }
